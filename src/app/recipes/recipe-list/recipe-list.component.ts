7b199586fafdfea09ec23e7c365d6da58d571530
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-07-08 21:28:37 +0100

    Build Recipe List & Recipe Item Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://images.ctfassets.net/uexfe9h31g3m/3Bo18bcgOQ2oyIUIE6S48Q/0b005fec068eff81fd657ba34627f894/Cottagepie_1024x768.jpg?w=1024&h=768&fm=webp&fit=thumb&q=90')
+  ];
 
   constructor() { }
 
